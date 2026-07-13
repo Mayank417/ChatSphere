f4f550fd92c9bab30a4a5d06b4b00f391b1868fa
Mayank417 <sainimayank7966980@gmail.com>
2026-07-13 14:49:45 +0530

    Update website title to ChatSphere

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-    req.headers = req.headers || {};
     req.headers.Authorization = `Bearer ${token}`;
   }
 
